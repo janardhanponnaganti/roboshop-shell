9fabf543cc5397662829b1e08c17e77feff58052
janardhanponnaganti <ponnagantijr@gmail.com>
2023-12-18 15:58:29 +0100

    roboshop shell scripts

---
+++ redis.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+Y="\e[33m"
+G="\e[32m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+exec &>$LOGFILE
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2... $R failed $N"
+        exit 1
+    else
+        echo -e "$2 ... $G success $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: plese run this script with root access $N"
+    exit 1 # you can give other then 0
+else
+    echo "you are root user"
+fi # fi means reverse of if, indicating condition end 
+
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y  &>> $LOGFILE
+
+VALIDATE $? "installing remi release"
+
+dnf module enable redis:remi-6.2 -y &>> $LOGFILE
+
+VALIDATE $? "enabling redis"
+
+dnf install redis -y
+
+VALIDATE $? "installing redis"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf
+
+VALIDATE $? "allowing remote connections"
+
+systemctl enable redis
+
+VALIDATE $? "enabled redis"
+
+systemctl start redis
+
+VALIDATE $? "staring redis"
---
+++ user.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+Y="\e[33m"
+G="\e[32m"
+N="\e[0m"
+MONGODB_HOST=mongodb.ponnaganti.online
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2... $R failed $N"
+        exit 1
+    else
+        echo -e "$2 ... $G success $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: plese run this script with root access $N"
+    exit 1 # you can give other then 0
+else
+    echo "you are root user"
+fi # fi means reverse of if, indicating condition end 
+
+dnf module disable nodejs -y  &>> $LOGFILE
+
+VALIDATE $? "disabling current nodeJs"
+
+dnf module enable nodejs:18 -y  &>> $LOGFILE
+
+VALIDATE $? "enabling nodeJS:18"
+
+dnf install nodejs -y  &>> $LOGFILE
+
+VALIDATE $? "installing nodeJS:18"
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd roboshop 
+    VALIDATE $? "roboshop user creation"
+ else 
+    echo -e "roboshop user already exit $Y SKIPPING $N"
+fi       
+
+mkdir -p /app
+
+VALIDATE $? "creating app directory" 
+
+curl -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip &>> $LOGFILE
+ 
+VALIDATE $? "Downloading user application" 
+
+cd /app
+
+unzip -o /tmp/user.zip &>> $LOGFILE
+
+VALIDATE $? "unzipping user" 
+
+npm install &>> $LOGFILE
+
+VALIDATE $? "installing dependencies" 
+
+#use absolute, because user.service exits there
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service &>> $LOGFILE
+
+VALIDATE $? "copying user service file" 
+
+systemctl daemon-reload &>> $LOGFILE
+
+VALIDATE $? "user demon reload" 
+
+systemctl enable user &>> $LOGFILE
+
+VALIDATE $? "enabling user"
+
+systemctl start user &>> $LOGFILE
+
+VALIDATE $? "starting user" 
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo 
+
+VALIDATE $? "copying mongodb repo"
+
+dnf install mongodb-org-shell -y &>> $LOGFILE
+
+VALIDATE $? "installing mongodb client" 
+
+mongo --host $MONGODB_HOST </app/schema/user.js &>> $LOGFILE
+
+VALIDATE $? "loading user data into MongoDB"
+
+
+
+
